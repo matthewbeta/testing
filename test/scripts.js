fe8338965801a5fe199226aec1f6f55b740a8201
matthewbeta <hello@matthew-jackson.com>
2015-06-11 22:35:58 +0100

    Final stage all tests written working and code written to make them pass

--- test/scripts.js
+++ test/scripts.js
@@ -1,3 +1,39 @@
 function add(a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return a + b;
+  } else {
+    throw new Error('Not a number');
+  }
+}
 
+function subtract(a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return a - b;
+  } else {
+    throw new Error('Not a number');
+  }
+}
+
+function multiply(a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return a * b;
+  } else {
+    throw new Error('Not a number');
+  }
+}
+
+function divide(a, b) {
+  if (isNumber(a) && isNumber(b)) {
+    return a / b;
+  } else {
+    throw new Error('Not a number');
+  }
+}
+
+function isNumber(n) {
+  if (typeof n === 'string' || n instanceof String) {
+    return false
+  } else {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
 }
--- test/spec/test.js
+++ test/spec/test.js
@@ -4,12 +4,59 @@
   'use strict';
 
   describe('Calculator', function() {
-    describe('add()', function() {
+    describe('add(a, b)', function() {
       it('a + b should be add up', function() {
-        assert.equal(3, add(1, 2));
-        assert.equal(-1, add(1, -2));
-        assert.equal(100, add(90, 10));
+        expect(add(2, 3)).to.equal(5);
+      });
+      it('should throw an error for non numbers', function() {
+        expect(add.bind(window, ['three', 'four'])).to.throw(
+          Error);
+        expect(add.bind(window, [3, 'four'])).to.throw(Error);
+        expect(add.bind(window, ['3', 4])).to.throw(Error);
       });
     });
+
+    describe('subtract(a, b)', function() {
+      it('a - b should work', function() {
+        expect(subtract(2, 3)).to.equal(-1);
+      });
+      it('should throw an error for non numbers', function() {
+        expect(subtract.bind(window, ['three', 'four'])).to.throw(
+          Error);
+        expect(subtract.bind(window, ['3', 4])).to.throw(Error);
+      });
+    });
+
+    describe('multiply(a, b)', function() {
+      it('a * b should work', function() {
+        expect(multiply(2, 3)).to.equal(6);
+      });
+      it('should throw an error for non numbers', function() {
+        expect(multiply.bind(window, ['three', 'four'])).to.throw(
+          Error);
+        expect(multiply.bind(window, ['3', 4])).to.throw(Error);
+      });
+    })
+
+    describe('divide(a, b)', function() {
+        it('a / b should work', function() {
+          expect(divide(6, 3)).to.equal(2);
+        })
+        it('should throw an error for non numbers', function() {
+          expect(divide.bind(window, ['three', 'four'])).to.throw(
+            Error);
+          expect(divide.bind(window, ['3', 4])).to.throw(Error);
+        });
+      })
+      //
+    describe('isNumber(n)', function() {
+      it('should return false if n is not a number', function() {
+        assert.isFalse(isNumber(undefined));
+        assert.isFalse(isNumber('string'));
+      })
+      it('should return true if n is a number', function() {
+        assert.isTrue(isNumber(1));
+      });
+    })
   });
 })();
